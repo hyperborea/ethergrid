b49c4b52ce1f001d0a8c2599f3f742a3792703a2
Sven Perkmann <sven.perkmann@klarna.com>
2015-11-29 21:50:57 +0100

    several improvements including performance at rest

--- ethergrid.js
+++ ethergrid.js
@@ -1,12 +1,14 @@
 Ethergrid = function(canvas, grid, options) {
-  var w = canvas.offsetWidth;
-  var h = canvas.offsetHeight;
+  var width  = canvas.offsetWidth;
+  var height = canvas.offsetHeight;
 
-  canvas.width = w;
-  canvas.height = h;
+  canvas.width  = width;
+  canvas.height = height;
 
   var ctx = canvas.getContext('2d');
   var points = [];
+  var isActive = true;
+  var activeCount = 0;
 
   options = options || {};
   _.defaults(options, {
@@ -28,13 +30,13 @@ Ethergrid = function(canvas, grid, options) {
 
   // PRIVATE
   function init (grid) {
-    _.invoke(points, 'destroy');
+    _.invoke(points, 'deactivate');
     points = [];
 
     for (var y = 0; y < grid.length; y++) {
       for (var x = 0; x < grid[0].length; x++) {
         if (grid[y][x]) {
-          var point = new Point((x+1) * options.padding, (y+1) * options.padding, options.size);
+          var point = new Point(x * options.padding + options.size, y * options.padding + options.size, options.size);
           points.push(point);
         }
       }
@@ -46,19 +48,13 @@ Ethergrid = function(canvas, grid, options) {
         .filter(function(p2) { return p1 != p2 && getDistance(p1, p2) <= Math.pow(options.padding, 2) * 2; })
         .value();
     });
-    // _.each(points, function(point) {
-    //   point.connections = _.chain(points)
-    //     .sortBy(function(p2) { return point.idx != p2.idx ? getDistance(point, p2) : Infinity; })
-    //     .first(2)
-    //     .value();
-    // });
   }
   
   function Point (x, y, r) {
     var _this = this;
     var _originX = x;
     var _originY = y;
-    var _active = true;
+    var _active = false;
 
     // constructor
     (function() {
@@ -67,7 +63,10 @@ Ethergrid = function(canvas, grid, options) {
       _this.r = r;
       _this.connections = [];
 
-      animate();
+      _this.deactivate = deactivate;
+      _this.activate = activate;
+
+      activate();
     })();
 
     // methods
@@ -93,28 +92,49 @@ Ethergrid = function(canvas, grid, options) {
       });
     };
 
-    this.destroy = function() {
-      _active = false;
+    function deactivate() {
+      if (_active) {
+        _active = false;
+        activeCount--;  
+      }
+    };
+
+    function activate() {
+      if (!_active) {
+        _active = true;
+        activeCount++;
+        animate();
+      }
     };
 
     // private
     function animate () {
+      if (!options.entropy && _this.x === _originX && _this.y === _originY) _this.deactivate();
+      if (!_active) return;
+
+      var targetX = _originX - options.entropy + options.entropy * 2 * Math.random();
+      var targetY = _originY - options.entropy + options.entropy * 2 * Math.random();
+
+      targetX = Math.min(Math.max(targetX, options.size), width - options.size);
+      targetY = Math.min(Math.max(targetY, options.size), height - options.size);
+
       TweenLite.to(_this, options.speed * (Math.random()+1), {
-        x: _originX - options.entropy + options.entropy * 2 * Math.random(),
-        y: _originY - options.entropy + options.entropy * 2 * Math.random(),
-        onComplete: function() {
-          if (_active) animate();
-        }
+        x: targetX,
+        y: targetY,
+        onComplete: animate
       });
     }
   }
 
 
   function render () {
-    ctx.clearRect(0, 0, w, h);
+    ctx.clearRect(0, 0, width, height);
     _.invoke(points, 'render');
 
-    requestAnimationFrame(render);
+    if (activeCount)
+      requestAnimationFrame(render);
+    else
+      isActive = false;
   }
 
   function getDistance(p1, p2) {
@@ -124,11 +144,22 @@ Ethergrid = function(canvas, grid, options) {
 
   // PUBLIC
   this.speed   = function(v) { options.speed = v; };
-  this.entropy = function(v) { options.entropy = v; };
+  this.entropy = function(v) {
+    options.entropy = v;
+    if (v) _.invoke(points, 'activate');
+    if (!isActive) {
+      isActive = true;
+      render();
+    }
+  };
   this.opacity = function(v) { options.color.a = v; };
   this.color   = function(r, g, b, speed) {
     speed = speed || 1;
-    TweenLite.to(options.color, speed, {r:r, g:g, b:b});
+    TweenLite.to(options.color, speed, {
+      r: r,
+      g: g,
+      b: b
+    });
   };
 
   this.colorHex = function(hex, speed) {
